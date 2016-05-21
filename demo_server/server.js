5962825a59888c5fe519903db43fc838e674378f
yangjiamei <yangjiamei@microtrust.com.cn>
2016-05-21 11:53:48 +0800

    add web page

--- demo_server/server.js
+++ demo_server/server.js
@@ -1,6 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n');
+    var content = '';
+    var type = '';
+    if(req.url === '/') {
+    content = fs.readFileSync('./index.html');
+    type = 'text/html';
+    } else if(req.url === '/base.css') {
+    content = fs.readFileSync('./base.css');
+    type = 'text/css';
+    }
+    res.writeHead(200, {'Content-Type': type});
+    res.end(content + '\n');
 }).listen(9000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:9000/');
